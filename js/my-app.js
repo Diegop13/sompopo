0df6691804da9c0919cc24fdc6f3a2c3466717ed
Diegop13 <39814633+Diegop13@users.noreply.github.com>
2018-09-19 18:24:31 -0600

    Upload 4

--- js/my-app.js
+++ js/my-app.js
@@ -22,6 +22,24 @@ var myApp = new Framework7({
 		path: '/productos/',
     	url: 'productos.html',
     	name: 'productos',
+  		},
+		
+		{
+		path: '/info-productouno/',
+    	url: 'info-productouno.html',
+    	name: 'info-productouno',
+  		},
+		
+		{
+		path: '/info-productodos/',
+    	url: 'info-productodos.html',
+    	name: 'info-productodos',
+  		},
+		
+		{
+		path: '/info-productotres/',
+    	url: 'info-productotres.html',
+    	name: 'info-productotres',
   		}
 	]
 });
